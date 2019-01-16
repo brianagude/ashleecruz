553eb445424ec7cfa9578608acd11864f36d1787
Briana Gude <briana.gude@gmail.com>
2019-01-16 13:01:40 -0500

    sizing & spacing

--- dist/js/show.js
+++ dist/js/show.js
@@ -1,57 +1,64 @@
 // change elements on scroll
-document.addEventListener('scroll', function() {
-  const pixels = window.pageYOffset
-  const sections = document.querySelectorAll('section.images')
-  const client = document.querySelector('div.client h2')
-  const desc = document.querySelector('div.desc p')
+document.addEventListener("scroll", function() {
+  const pixels = window.pageYOffset;
+  const sections = document.querySelectorAll("section.images");
+  const client = document.querySelector("div.client h2");
+  const desc = document.querySelector("div.desc p");
 
   sections.forEach(section => {
-    if (section.offsetTop - 200 <= pixels){
-      client.innerHTML = section.getAttribute('data-client')
-      desc.innerHTML = section.getAttribute('data-desc')
-      document.body.style.backgroundColor = section.getAttribute('data-bkgrd')
+    if (section.offsetTop - 200 <= pixels) {
+      client.innerHTML = section.getAttribute("data-client");
+      desc.innerHTML = section.getAttribute("data-desc");
+      document.body.style.backgroundColor = section.getAttribute("data-bkgrd");
     }
-  })
-})
-
+  });
+});
 
 // contentful
-const body = document.body
+const body = document.body;
 
-const spaceid = 'nbf7jcn8mztl'
-const environmentid = 'master'
-const accesstoken = '358a5b67b07990394c3d9b1bdfccfc72e62a70cee0ecee048ed5100a52865765'
-const url = `https://cdn.contentful.com/spaces/${spaceid}/environments/${environmentid}/entries?access_token=${accesstoken}&order=-fields.client&content_type=clientName`
+const spaceid = "nbf7jcn8mztl";
+const environmentid = "master";
+const accesstoken =
+  "358a5b67b07990394c3d9b1bdfccfc72e62a70cee0ecee048ed5100a52865765";
+const url = `https://cdn.contentful.com/spaces/${spaceid}/environments/${environmentid}/entries?access_token=${accesstoken}&order=-fields.client&content_type=clientName`;
 
-const projectData = function (){
+const projectData = function() {
   return fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    return data.items.map(item => {
-      item.fields.photoString = item.fields.photos.map(photo => {
-        let asset = data.includes.Asset.find(asset => {
-          return asset.sys.id == photo.sys.id
-        })
+    .then(response => response.json())
+    .then(data => {
+      return data.items.map(item => {
+        item.fields.photoString = item.fields.photos
+          .map(photo => {
+            let asset = data.includes.Asset.find(asset => {
+              return asset.sys.id == photo.sys.id;
+            });
 
-        let url = asset.fields.file.url
-        return url
-      }).map(url => `<img src="${url}">`).join("")
-      
-      return item.fields
-    })
-  })
-}
+            let url = asset.fields.file.url;
+            return url;
+          })
+          .map(url => `<img src="${url}">`)
+          .join("");
+
+        return item.fields;
+      });
+    });
+};
 
 projectData().then(data => {
-  console.log(data)
+  console.log(data);
 
-  data.forEach(item =>{
-   body.innerHTML = body.innerHTML + `
-   <section class='images' id='${item.client}' data-client = '${item.client}' data-desc='${item.description}' data-bkgrd='${item.backgroundColor}'>
-      <div class='content'>
-        ${item.photoString}
-      </div>
-    </section>
- `
- })
-})
+  data.forEach(item => {
+    body.innerHTML =
+      body.innerHTML +
+      `
+        <section class='images' id='${item.client}' data-client = '${
+        item.client
+      }' data-desc='${item.description}' data-bkgrd='${item.backgroundColor}'>
+        <div class='content'>
+          ${item.photoString}
+        </div>
+      </section>
+    `;
+  });
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,67 +1,72 @@
 // dropdown sentence
-const button = document.querySelectorAll('div.list')
-const list = document.querySelectorAll('div.button')
-const projectLinks = document.querySelectorAll('section.projects div')
+const button = document.querySelectorAll("div.list");
+const list = document.querySelectorAll("div.button");
+const projectLinks = document.querySelectorAll("section.projects div");
+const ashlee = document.querySelector("div.ashlee img");
 
-$(button).on('mouseenter', function(){
-	$(list).css("display", "inline");
-	$(projectLinks).css("opacity", "0.1");
-})
+$(button).on("mouseenter", function() {
+  $(list).css("display", "inline");
+  $(projectLinks).css("opacity", "0.1");
+  $(ashlee).css("opacity", "1");
+});
 
-$(button).on('mouseout', function(){
-	$(list).css("display", "none");
-	$(projectLinks).css("opacity", "1");
-})
+$(button).on("mouseout", function() {
+  $(list).css("display", "none");
+  $(projectLinks).css("opacity", "1");
+  $(ashlee).css("opacity", "0");
+});
 
-$(button).on('touchstart', function(){
-	$(list).css("display", "inline");
-	$(projectLinks).css("opacity", "0.1");
-})
+$(button).on("touchstart", function() {
+  $(list).css("display", "inline");
+  $(projectLinks).css("opacity", "0.1");
+  $(ashlee).css("opacity", "1");
+});
 
-$(button).on('touchend', function(){
-	$(list).css("display", "none");
-	$(projectLinks).css("opacity", "1");
-})
+$(button).on("touchend", function() {
+  $(list).css("display", "none");
+  $(projectLinks).css("opacity", "1");
+  $(ashlee).css("opacity", "0");
+});
 
 // contentful
-const projects = document.querySelector('section.projects div')
+const projects = document.querySelector("section.projects div");
 
-const spaceid = 'nbf7jcn8mztl'
-const environmentid = 'master'
-const accesstoken = '358a5b67b07990394c3d9b1bdfccfc72e62a70cee0ecee048ed5100a52865765'
-const url = `https://cdn.contentful.com/spaces/${spaceid}/environments/${environmentid}/entries?access_token=${accesstoken}&order=-fields.client&content_type=clientName`
+const spaceid = "nbf7jcn8mztl";
+const environmentid = "master";
+const accesstoken =
+  "358a5b67b07990394c3d9b1bdfccfc72e62a70cee0ecee048ed5100a52865765";
+const url = `https://cdn.contentful.com/spaces/${spaceid}/environments/${environmentid}/entries?access_token=${accesstoken}&order=-fields.client&content_type=clientName`;
 
-const projectData = function (){
+const projectData = function() {
   return fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    return data.items.map(item => {
-      item.fields.photoUrls = item.fields.photos.map(photo => {
-        let asset = data.includes.Asset.find(asset => {
-          return asset.sys.id == photo.sys.id
-        })
+    .then(response => response.json())
+    .then(data => {
+      return data.items.map(item => {
+        item.fields.photoUrls = item.fields.photos.map(photo => {
+          let asset = data.includes.Asset.find(asset => {
+            return asset.sys.id == photo.sys.id;
+          });
 
-        let url = asset.fields.file.url
-        return url
-      })
+          let url = asset.fields.file.url;
+          return url;
+        });
 
-      return item.fields
-    })
-
-
-
-  })
-}
+        return item.fields;
+      });
+    });
+};
 
 projectData().then(data => {
-  console.log(data)
+  console.log(data);
 
-  data.forEach(item =>{
-   projects.innerHTML = projects.innerHTML + `
+  data.forEach(item => {
+    projects.innerHTML =
+      projects.innerHTML +
+      `
     <a href='show.html'>
       ${item.client}
       <img src='${item.photoUrls[0]}'>
     </a>
- `
- })
-})
+ `;
+  });
+});
